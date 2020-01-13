00850e38f6b11cadc24c3399fe994a51771513f4
Dmitriy Bagandov <dmitriy.bagandov@gmail.com>
2020-01-13 21:49:47 +0200

    Add second timer showing total time

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Timer from "./Timer";
 const bodyParts = [
   "forearm",
   "shoulder",
-  "calves",
-  "hip",
-  "stomach",
-  "chest",
-  "neck",
-  "lips",
-  "eyebrows",
-  "forehead"
+  "calves"
+  // "hip",
+  // "stomach",
+  // "chest",
+  // "neck",
+  // "lips",
+  // "eyebrows",
+  // "forehead"
 ];
-const activationTime = 5;
-const relaxingTime = 15;
+const activationTime = 1;
+const relaxingTime = 2;
 
 function App() {
+  const [isSessionEnd, setIsSessionEnd] = useState(false);
   const [partIndex, setPartIndex] = useState(0);
   const [isRelaxing, setIsRelaxing] = useState(false);
 
@@ -25,14 +26,27 @@ function App() {
     setIsRelaxing(!isRelaxing);
   }
 
+  function endSession() {
+    setIsSessionEnd(true);
+  }
+
+  let statusText;
+  if (isSessionEnd) statusText = <p>Session ended, good job!</p>;
+  else if (isRelaxing) statusText = <p>Relax...</p>;
+  else statusText = <p>Activate {bodyParts[partIndex]} muscles</p>;
+
   return (
     <div className="App">
       <Timer
         timeLeft={isRelaxing ? relaxingTime : activationTime}
+        allTimeLeft={
+          (bodyParts.length + 1) * (activationTime + relaxingTime) +
+          relaxingTime
+        }
         onTimerUpdate={setNextBodyPart}
+        onTimerEnd={endSession}
       />
-      {isRelaxing && <p>Relax...</p>}
-      {!isRelaxing && <p>Activate {bodyParts[partIndex]} muscles</p>}
+      {statusText}
     </div>
   );
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useRef, useState } from "react";
 
 function Timer(props) {
-  const [time, setTime] = useState(props.timeLeft || 60);
+  const [allTime, setAllTime] = useState(props.allTimeLeft);
+  const [time, setTime] = useState(props.timeLeft);
+  const [isOn, turnOn] = useState(false);
   const savedOnTimerUpdate = useRef();
+  const savedOnTimerEnd = useRef();
 
   function startTimer() {
     turnOn(true);
   }
 
+  // save this callback here so that changes of props don't trigger other useEffect
   useEffect(() => {
     savedOnTimerUpdate.current = props.onTimerUpdate;
-  }, [props.onTimerUpdate]);
+    savedOnTimerEnd.current = props.onTimerEnd;
+  }, [props.onTimerUpdate, props.onTimerEnd]);
 
   useEffect(() => {
+    if (!isOn) return;
+
     setTimeout(() => {
       if (time === 0) {
         savedOnTimerUpdate.current();
@@ -20,15 +27,27 @@ function Timer(props) {
       }
       setTime(time - 1);
     }, 1000);
-  }, [time]);
+  }, [time, isOn]);
 
+  useEffect(() => {
+    if (!isOn) return;
+
+    setTimeout(() => {
+      if (allTime === 0) {
+        turnOn(false);
+        savedOnTimerEnd.current();
+        return;
+      }
+      setAllTime(allTime - 1);
+    }, 1000);
+  }, [allTime, isOn]);
+
+  // this gets called when props are changed after call of saveOnTimerUpdate.current()
   useEffect(() => {
     setTime(props.timeLeft);
   }, [props.timeLeft]);
 
-  const [isOn, turnOn] = useState(false);
-
-  function getOverallTimeLeft(secs) {
+  function getTimeInString(secs) {
     var hours = Math.floor(secs / 3600);
     var minutes = Math.floor((secs - hours * 3600) / 60);
     var seconds = secs - hours * 3600 - minutes * 60;
@@ -41,7 +60,8 @@ function Timer(props) {
 
   return (
     <div>
-      <p className="overallTime">{getOverallTimeLeft(time)}</p>
+      <p className="overallTimer">Total: {getTimeInString(allTime)}</p>
+      <p className="subTimer">{getTimeInString(time)}</p>
       <p className="currentExercise">{}</p>
       {!isOn && <button onClick={startTimer}>Start session</button>}
     </div>
